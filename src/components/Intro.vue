b84910b1796f89cf166c9c0631f724148a4a969a
tae8838 <tae@wovn.io>
2018-09-24 19:08:58 +0900

    init

---
+++ src/components/Intro.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="intro">
+    <h1>Tae Noppakun Wongsrinoppakun</h1>
+  </div>
+</template>
+<script>
+
+</script>
+<style>
+.prof-pic-img {
+  margin: 0px 25px;
+  border-radius: 50%;
+  height: 50px;
+}
+
+.intro {
+  display: inline-flex;
+}
+.intro {
+    /*background-color: #333;*/
+    overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.intro a {
+    float: left;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+/* Change the color of links on hover */
+.intro h1:hover {
+  font-size: 500px;
+  transition-duration: 20000ms;
+}
+</style>
